6822f8786e003cf4e253db31475ff55dc9c12dad
Natalia <natalia@cloudcoreo.com>
2017-01-12 16:59:21 +0600

    test

--- services/config.rb
+++ services/config.rb
@@ -1,50 +1,21 @@
-######################################################################
-## variables
-##   PUBLIC_ROUTE_NAME
-##   PUBLIC_SUBNET_NAME
-##   VPC_NAME
-##   VPC_OCTETS
-######################################################################
-
-######################################################################
-## Create a vpc, igw and attach the igw to the vpc
-######################################################################
-coreo_aws_vpc_vpc "${VPC_NAME}${SUFFIX}" do
-  action :sustain
-  cidr "${VPC_OCTETS}/16"
-  internet_gateway true
-  region "${REGION}"
-  tags ${VPC_TAGS}
+coreo_uni_util_notify "advise-s3-json" do
+  action :nothing
+  type 'email'
+  allow_empty ${AUDIT_AWS_S3_ALLOW_EMPTY}
+  send_on '${AUDIT_AWS_S3_SEND_ON}'
+  payload '{"composite name":"PLAN::stack_name",
+  "plan name":"PLAN::name",
+  "number_of_checks":"COMPOSITE::coreo_aws_advisor_s3.advise-s3.number_checks",
+  "number_of_violations":"COMPOSITE::coreo_aws_advisor_s3.advise-s3.number_violations",
+  "number_violations_ignored":"COMPOSITE::coreo_aws_advisor_s3.advise-s3.number_ignored_violations",
+  "violations": COMPOSITE::coreo_aws_advisor_s3.advise-s3.report }'
+  payload_type "json"
+  endpoint ({
+      :to => 'test', :subject => 'CloudCoreo s3 advisor alerts on PLAN::stack_name :: PLAN::name'
+  })
 end
 
-
-######################################################################
-## create a routetable for the public subnet, route everything
-## to the internet gateway
-######################################################################
-coreo_aws_vpc_routetable "${PUBLIC_ROUTE_NAME}${SUFFIX}" do
-  action :sustain
-  vpc "${VPC_NAME}${SUFFIX}"
-  routes [
-             { :from => "0.0.0.0/0", :to => "${VPC_NAME}${SUFFIX}", :type => :igw }
-        ]
-  number_of_tables 1
-  region "${REGION}"
-end
-
-
-######################################################################
-## number_of_zones
-##   cidr will be split up among all zones specified in "number_of_zones"
-## percent_of_vpc_allocated
-##   split, but use only this percentage of the entire vpc range
-######################################################################
-coreo_aws_vpc_subnet "${PUBLIC_SUBNET_NAME}${SUFFIX}" do
-  action :sustain
-  number_of_zones ${PUBLIC_SUBNET_NUM_ZONES}
-  percent_of_vpc_allocated 25
-  route_table "${PUBLIC_ROUTE_NAME}${SUFFIX}"
-  vpc "${VPC_NAME}${SUFFIX}"
-  map_public_ip_on_launch true
-  region "${REGION}"
+coreo_uni_util_notify "advise-s3-to-tag-values" do
+  action :nothing
+  notifiers 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-s3.return'
 end

4270fa5380a67afdbd061353f76198698453acb7
Natalia <natalia@cloudcoreo.com>
2017-03-30 11:54:07 +0600

    test global

--- services/config.rb
+++ services/config.rb
@@ -1,3 +1,9 @@
 coreo_uni_util_variables "resource_25" do
   action :nothing
 end
+coreo_uni_util_variables "test-global" do
+  action :set
+  variables([
+                {'GLOBAL::test_var' => 'lalalala'}
+            ])
+end

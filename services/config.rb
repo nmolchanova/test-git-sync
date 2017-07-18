3870f2999fbd826fc41882f06bc4d1f8f422fe1c
Natalia <natalia@cloudcoreo.com>
2017-07-18 11:23:03 +0600

    test

--- services/config.rb
+++ services/config.rb
@@ -8,7 +8,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-rds" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.10.7-9"
+                   :version => "1.10.7-10"
                },
                {
                    :name => "js-yaml",

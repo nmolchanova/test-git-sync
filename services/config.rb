e0ad20d6e2c929045e487223020673b3a64711c2
Natalia <natalia@cloudcoreo.com>
2017-07-20 11:22:58 +0600

    test

--- services/config.rb
+++ services/config.rb
@@ -1,43 +1,51 @@
-# test1
-## test2
-### test3
-coreo_uni_util_jsrunner "tags-to-notifiers-array-rds" do
+coreo_aws_rule "cloudtrail-logs-encrypted" do
+  action :define
+  service :user
+  category "Audit"
+  link "http://kb.cloudcoreo.com/mydoc_cloudtrail-logs-encrypted.html"
+  display_name "Verify CloudTrail logs are encrypted at rest using KMS CMKs"
+  suggested_action "It is recommended that CloudTrail be configured to use SSE-KMS."
+  description "AWS CloudTrail is a web service that records AWS API calls for an account and makes those logs available to users and resources in accordance with IAM policies. AWS Key Management Service (KMS) is a managed service that helps create and control the encryption keys used to encrypt account data, and uses Hardware Security Modules (HSMs) to protect the security of encryption keys. CloudTrail logs can be configured to leverage server side encryption (SSE) and KMS customer created master keys (CMK) to further protect CloudTrail logs."
+  level "Medium"
+  meta_cis_id "2.7"
+  meta_cis_scored "true"
+  meta_cis_level "2"
+  meta_nist_171_id "3.3.1"
+  objectives [""]
+  audit_objects [""]
+  operators [""]
+  raise_when [true]
+  id_map "static.no_op"
+end
+
+coreo_aws_rule_runner "cloudtrail-inventory-runner" do
+  action :run
+  service :cloudtrail
+  rules ["cloudtrail-logs-encrypted"]
+end
+
+coreo_uni_util_jsrunner "test-runner" do
   action :run
   data_type "json"
   provide_composite_access true
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.10.7-10"
+                   :version => "*"
                },
                {
                    :name => "js-yaml",
                    :version => "3.7.0"
                }
                   ])
-  json_input '{}'
+  json_input '{"violations":COMPOSITE::coreo_aws_rule_runner.advise-cloudtrail.report}'
   function <<-EOH
-function setTableAndSuppression() {
-  let table;
-  let suppression;
-  const fs = require('fs');
-  const yaml = require('js-yaml');
-  try {
-      suppression = yaml.safeLoad(fs.readFileSync('./suppression.yaml', 'utf8'));
-  } catch (e) {
-      console.log("Error reading suppression.yaml file: " , e);
-      suppression = {};
-  }
-  try {
-      table = yaml.safeLoad(fs.readFileSync('./table.yaml', 'utf8'));
-  } catch (e) {
-      console.log("Error reading table.yaml file: ", e);
-      table = {};
-  }
-  coreoExport('table', JSON.stringify(table));
-  coreoExport('suppression', JSON.stringify(suppression));
-}
-setTableAndSuppression();
-callback();
+    newJSONInput.violations = {};
+    callback(newJSONInput.violations);
   EOH
+end
+
+coreo_uni_util_variables "cloudtrail-update-planwide-2" do
+  action :set
+  variables([ {'COMPOSITE::coreo_aws_rule_runner.cloudtrail-inventory-runner.report' => 'COMPOSITE::coreo_uni_util_jsrunner.test-runner.return'} ])
 end
